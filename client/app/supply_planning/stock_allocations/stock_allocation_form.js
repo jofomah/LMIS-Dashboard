fe050bbdcc56d18d49c75ce1c0d54ac268bc6681
lordmallam <lordy002000@gmail.com>
2016-05-13 20:18:42 +0100

    Supply Planning Prototype - Dashboard

---
+++ client/app/supply_planning/stock_allocations/stock_allocation_form.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+angular.module('lmisApp')
+  .config(function ($routeProvider) {
+      $routeProvider
+        .when('/supply-planning', {
+            templateUrl: 'app/supply_planning/supply_planning.html',
+            controller: 'SupplyPlanningCtrl',
+            authenticate: true,
+
+        });
+  })
+.controller("StockAllocationFormCtrl", function ($scope, $http, $modal) {
+    debugger
+   
+    $scope.selectedFacility = null;
+    $http.get('app/supply_planning/assignment.json')
+        .success(function (data) {
+            $scope.assignmentContents = data;
+        });
+   
+    
+});
---
+++ client/app/supply_planning/supply_planning.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+angular.module('lmisApp')
+  .config(function ($routeProvider) {
+      $routeProvider
+        .when('/supply-planning', {
+            templateUrl: 'app/supply_planning/supply_planning.html',
+            controller: 'SupplyPlanningCtrl',
+            authenticate: true,
+            
+        });
+  })
+.controller("SupplyPlanningCtrl", function ($scope, $http) {
+    debugger
+    $scope.contents = null;
+    $scope.assignmentContents = null;
+    $http.get('app/supply_planning/assignment.json')
+        .success(function (data) {
+            $scope.assignmentContents = data;
+        });
+    $http.get('app/supply_planning/dummy.json')
+        .success(function (data) {
+            $scope.contents = data;
+        });
+
+    $http.get('app/supply_planning/vendors.json')
+        .success(function (data) {
+            $scope.vendorContents = data;
+        });
+    
+    $scope.showAllocationForm = function () {
+        $scope.State = cRow.State;
+        $modal.open({
+            templateUrl: 'app/supply_planning/stock_allocations/stock-allocation-form.html',
+            controller: 'StockAllocationFormCtrl'
+        });
+    };
+});
