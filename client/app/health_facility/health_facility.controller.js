9e40aede5c5d4dd10c37ecca46127ef85a59422f
Lord-Mallam <Lord-Mallam@DESKTOP-989V4AK>
2016-09-09 09:32:11 +0100

    Health Facility page inclusion - app/health_facility

---
+++ client/app/health_facility/health_facility.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('lmisApp')
+  .controller('healthFacilityCtrl', function ($scope, $filter, utility, Auth, Pagination, Places) {
+    
+  });
--- client/app/stock_count/stock_count.controller.js
+++ client/app/stock_count/stock_count.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('lmisApp')
-  .controller('StockCountCtrl', function($scope, $filter, $routeParams, utility, Auth, Pagination, Places, stockCounts, productTypes, UoMs) {
+  .controller('StockCountCtrl', function ($scope, $filter, $routeParams, utility, Auth, Pagination, stockCounts, Places, productTypes, UoMs, $http) {
     var rows = stockCounts;
     $scope.currentUser = Auth.getCurrentUser();
     $scope.productTypes = productTypes;
@@ -11,6 +11,7 @@ angular.module('lmisApp')
     $scope.places = null;
     $scope.UoMs = UoMs;
     $scope.getFilename = utility.getFileName;
+    $scope.chartTab = true;
     $scope.csvHeader = [
       'State',
       'Zone',
@@ -127,4 +128,168 @@ angular.module('lmisApp')
     }
 
     $scope.update();
+
+
+    //new  UI
+    $scope.summaryOptions = {
+      chart: {
+        type: 'multiBarHorizontalChart',
+        legend: {
+          margin: {
+            top: 5,
+            right: 35,
+            bottom: 5,
+            left: 0
+          },
+          maxKeyLength: 500,
+          align: true,
+          height: 250,
+          padding: 50,
+          rightAlign: false
+        },
+        height: 120,
+        stacked: true,
+        x: function (d) { return d.label; },
+        y: function (d) { return d.value; },
+        //yErr: function(d){ return [-Math.abs(d.value * Math.random() * 0.3), Math.abs(d.value * Math.random() * 0.3)] },
+        showControls: false,
+        showValues: true,
+        showXAxis: false,
+        duration: 500,
+        yAxis: {
+          ticks: 10,
+          tickFormat: function (d) { return d3.format(',p')(d / 100); }
+        }
+      }
+    };
+
+    $scope.summaryData = [
+            {
+              "key": "All antigens sufficient",
+              "color": "#005d98",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 60
+                  }
+              ]
+            },
+            {
+              "key": "1-2 antigens below minimum",
+              "color": "#00d5d2",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 20
+                  }
+              ]
+            },
+            {
+              "key": "3 or more antigens below minimum",
+              "color": "#ff7e00",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 15
+                  }
+              ]
+            },
+            {
+              "key": "LGAs that did not report",
+              "color": "#af00d4",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 5
+                  }
+              ]
+            }
+    ];
+
+    $scope.summaryDataInventory = [
+            {
+              "key": "All commodities sufficient",
+              "color": "#01c256",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 60
+                  }
+              ]
+            },
+            {
+              "key": "1-2 commodities below minimum",
+              "color": "#f8e71c",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 20
+                  }
+              ]
+            },
+            {
+              "key": "3 or more commodities below minimum",
+              "color": "#f84324",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 15
+                  }
+              ]
+            },
+            {
+              "key": "Non reporting",
+              "color": "#D3D3D3",
+              "values": [
+                  {
+                    "label": "Summary",
+                    "value": 5
+                  }
+              ]
+            }
+    ];
+
+
+
+    $scope.minOptions = {
+      chart: {
+        type: 'multiBarHorizontalChart',
+        height: 100,
+        stacked: true,
+        x: function (d) { return d.label; },
+        y: function (d) { return d.value; },
+        showControls: false,
+        showValues: true,
+        showXAxis: true,
+        duration: 500,
+        showLegend: false,
+        yAxis: {
+          ticks: 10,
+          tickFormat: function (d) { return d3.format(',p')(d / 100); }
+        }
+      }
+    };
+
+    $http.get('app/sample_data/stock-count-lgas.json')
+        .success(function (data) {
+          $scope.lgas1 = [];
+          $scope.lgas2 = [];
+          for (var i = 0; i < data[0].LGAs.length; i++) {
+            if (isEven(i)) {
+              $scope.lgas1.push(data[0].LGAs[i]);
+            } else {
+              $scope.lgas2.push(data[0].LGAs[i]);
+            }
+          }
+        });
+
+    $scope.tabview = function (v) {
+      $scope.chartTab = v;
+    }
+
+    function isEven(n) {
+      return n % 2 == 0;
+    }
+
+
   });
