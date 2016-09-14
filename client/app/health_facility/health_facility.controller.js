6b3b7cc5ac5a8abf0c0da93742f5cbbab054191d
princess chike-ogwo <princess.chike-ogwo@ehealthnigeria.org>
2016-09-14 09:49:23 +0100

    Angularization of health_facility

--- client/app/health_facility/health_facility.controller.js
+++ client/app/health_facility/health_facility.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('lmisApp')
-  .controller('healthFacilityCtrl', function ($scope, $filter, utility, Auth, Pagination, Places) {
-    
+  .controller('HealthFacilityCtrl', function ($scope, $filter, utility, Auth, Pagination, Places) {
+    $scope.products = [
+      {product: "IPV", count: "15", uom: "doses", maximumLevel: "120", minimumLevel: "0", reorderLevel: "20"},
+      {product: "Phytomenadione", count: "0", uom: "vials", maximumLevel: "50", minimumLevel: "2", reorderLevel: "5"},
+      {product: "ADS 0.05ml", count: "120", uom: "doses", maximumLevel: "150", minimumLevel: "40", reorderLevel: "20"},
+    ]
+    $scope.count = 0;
+    $scope.default = function (product) {return $scope.count == undefined;};
+    $scope.green = function (product) {return $scope.count >= product.maximumLevel;};
+    $scope.yellow = function (product) {return $scope.count < product.reorderLevel;};
+    $scope.red = function (product) {return $scope.count < product.minimumLevel;};
   });
