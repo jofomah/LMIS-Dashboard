662edf84fe1da6458eca19783910d229ea830750
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-20 14:43:00 +0100

    move jquery sliding menu to app.js config section

--- client/app/app.js
+++ client/app/app.js
@@ -27,6 +27,36 @@ angular.module('lmisApp',
 						redirectTo: '/'
 					});
 
+      jQuery("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        jQuery("#wrapper").toggleClass("toggled");
+      });
+      jQuery("#menu-toggle-2").click(function (e) {
+        e.preventDefault();
+        jQuery("#wrapper").toggleClass("toggled-2");
+        jQuery('#menu ul').hide();
+      });
+
+      function initMenu() {
+        jQuery('#menu ul').hide();
+        jQuery('#menu ul').children('.current').parent().show();
+        //jQuery('#menu ul:first').show();
+        jQuery('#menu li a').click(
+          function () {
+            var checkElement = jQuery(this).next();
+            if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+              return false;
+            }
+            if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+              jQuery('#menu ul:visible').slideUp('normal');
+              checkElement.slideDown('normal');
+              return false;
+            }
+          }
+        );
+      }
+      jQuery(document).ready(function () { initMenu(); });
+
 			$locationProvider.html5Mode(true);
 			$httpProvider.interceptors.push('authInterceptor');
 			uiSelectConfig.theme = 'bootstrap';
