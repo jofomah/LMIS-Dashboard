06f2f2fa0cc7965bcbb8f9dc66be0e0029e5613d
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-09-28 08:52:56 +0100

    refactor location module to use db-service

--- server/api/location/location.model.js
+++ server/api/location/location.model.js
@@ -1,23 +1,12 @@
 'use strict';
 
-var cradle = require('cradle');
-var utility = require('../../components/utility');
-var config = require('../../config/environment');
+var dbService = require('../../components/db');
+var VIEWS = require('../../components/db/db-constants').VIEWS;
 
-var db = new (cradle.Connection)().database(config.couch.db);
 var doc_type = 'location'
 
-exports.all = all;
 
-
-
-function all(cb) {
-  var options = {
-    include_docs: true,
-    key: doc_type
-  }
-  db.view('doctypes/by-type', options, function (err, res) {
-    if (err) return cb(err);
-    return cb(null, res.toArray());
-  });
-}
+exports.all = function (options) {
+  var opts = options || {include_docs: true, key: doc_type}
+  return dbService.queryBy(VIEWS.byDocTypes, opts)
+};
