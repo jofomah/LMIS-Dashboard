cd43c61851825ef9cc328bf07546d25ce4ffa17f
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-10-14 15:03:17 +0100

    add product-category payload schema (Joi validation)

---
+++ server/api/product_category/product_category.schema.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var Joi = require('joi');
+var DOC_TYPES = require('../../components/db/db-constants').DOC_TYPES;
+
+exports.createSchema = Joi.object().keys({
+  name: Joi.string().min(1).max(35).required(),
+  parent: Joi.string().default('').optional(),
+  is_deleted: Joi.boolean().default(false)
+});
+
+exports.updateSchema = Joi.object().keys({
+  name: Joi.string().min(1).max(35).required(),
+  parent: Joi.string().default('').optional(),
+  is_deleted: Joi.boolean().default(false),
+  _id: Joi.string().required(),
+  _rev: Joi.string().required(),
+  doc_type: Joi.string().default(DOC_TYPES.productCategory).valid(DOC_TYPES.productCategory),
+  created: Joi.date().iso().required(),
+  modified: Joi.date().iso().optional()
+});
