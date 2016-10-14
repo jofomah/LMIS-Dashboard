fa17f572335e84750e7b39ae99bd52990a4cf180
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-10-14 17:52:29 +0100

    updated prod-category schema, default parent to null

--- server/api/product_category/product_category.schema.js
+++ server/api/product_category/product_category.schema.js
@@ -5,7 +5,7 @@ var DOC_TYPES = require('../../components/db/db-constants').DOC_TYPES;
 
 exports.createSchema = Joi.object().keys({
   name: Joi.string().min(1).max(35).required(),
-  parent: Joi.string().default('').optional(),
+  parent: Joi.string().default(null).optional(),
   is_deleted: Joi.boolean().default(false)
 });
 
@@ -17,5 +17,5 @@ exports.updateSchema = Joi.object().keys({
   _rev: Joi.string().required(),
   doc_type: Joi.string().default(DOC_TYPES.productCategory).valid(DOC_TYPES.productCategory),
   created: Joi.date().iso().required(),
-  modified: Joi.date().iso().optional()
+  modified: Joi.date().iso().default(null).optional()
 });
