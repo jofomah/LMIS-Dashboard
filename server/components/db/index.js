d2eec78ec0db40420451558ecbfa642cedbfcb48
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-10-14 15:15:19 +0100

    add db-service get record by id and add time-stamp before saving.

--- server/components/db/index.js
+++ server/components/db/index.js
@@ -11,13 +11,35 @@ var db = new (cradle.Connection)().database(config.couch.db);
 // see: http://bluebirdjs.com/docs/working-with-callbacks.html#working-with-callback-apis-using-the-node-convention
 db = Promise.promisifyAll(db);
 
-
-function toArray(cradleResultSet) {
-  return cradleResultSet.toArray();
-}
+exports.addTimeInfo = addTimeInfo;
 
 exports.queryBy = function (view, options) {
   var opts = options || {}
   return db.viewAsync(view, opts)
     .then(toArray);
 };
+
+exports.save = function (doc) {
+  doc = addTimeInfo(doc);
+  if (doc._rev) {
+    return db.saveAsync(doc._id, doc._rev, doc);
+  }
+  return db.saveAsync(doc._id, doc);
+};
+
+exports.get = function (id) {
+  return db.getAsync(id);
+};
+
+function toArray(cradleResultSet) {
+  return cradleResultSet.toArray();
+}
+
+function addTimeInfo (doc) {
+  var now = new Date().toJSON();
+  if (!doc.created) {
+    doc.created = now;
+  }
+  doc.modified = now;
+  return doc;
+}
