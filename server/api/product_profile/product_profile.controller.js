e6000956a1ae433a4474f94e8d21d2458d6130c0
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-10-17 10:37:20 +0100

    create product profile API request handlers

--- server/api/product_profile/product_profile.controller.js
+++ server/api/product_profile/product_profile.controller.js
@@ -1,13 +1,55 @@
 'use strict';
 
 var ProductProfile = require('./product_profile.model');
-var config = require('../../config/environment');
+var Utility = require('../../components/utility');
+var productProfileSchema = require('./product_profile.schema');
+var Promise = require('bluebird');
+var Joi = Promise.promisifyAll(require('joi'));
 
 // get list of product profiles
 exports.index = function(req, res, next) {
-  ProductProfile.all(function(err, profiles) {
-    if (err) return next(err);
-
-    res.json(profiles);
-  });
+  ProductProfile.all()
+    .then(res.json.bind(res))
+    .catch(function (err) {
+      Utility.handleError(err, res);
+    })
+    .catch(next)
 };
+
+exports.get = function (req, res, next) {
+  var id = req.params.id;
+  return ProductProfile.get(id)
+    .then(res.json.bind(res))
+    .catch(function (err) {
+      Utility.handleError(err, res);
+    })
+    .catch(next);
+};
+
+exports.create = function (req, res, next) {
+  var payload = req.body;
+  Joi.validateAsync(payload, productProfileSchema.createSchema)
+    .then(ProductProfile.create)
+    .then(getDocFromSaveResult)
+    .then(res.json.bind(res))
+    .catch(function (err) {
+      Utility.handleError(err, res);
+    })
+    .catch(next);
+}
+
+exports.save = function (req, res, next) {
+  var payload = req.body;
+  Joi.validateAsync(payload, productProfileSchema.updateSchema)
+    .then(ProductProfile.save)
+    .then(getDocFromSaveResult)
+    .then(res.json.bind(res))
+    .catch(function (err) {
+      Utility.handleError(err, res);
+    })
+    .catch(next);
+};
+
+function getDocFromSaveResult (result) {
+  return ProductProfile.get(result.id);
+}
