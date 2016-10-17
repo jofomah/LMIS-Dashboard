17afcdd17cd5d637a351fa88a20c845b8d42800b
jofomah <jideobi.ofomah@ehealthnigeria.org>
2016-10-17 10:36:59 +0100

    extend product profile model to create, get and save product profile records

--- server/api/product_profile/product_profile.model.js
+++ server/api/product_profile/product_profile.model.js
@@ -1,16 +1,35 @@
 'use strict';
 
-var cradle = require('cradle');
-var utility = require('../../components/utility');
+var dbService = require('../../components/db');
+var VIEWS = require('../../components/db/db-constants').VIEWS;
+var DOC_TYPES = require('../../components/db/db-constants').DOC_TYPES;
 
-var db = new (cradle.Connection)().database('product_profiles');
 
+// expose public functions
 exports.all = all;
+exports.get = get;
+exports.create = create;
+exports.save = save;
 
-function all(cb) {
-  db.all({ include_docs: true }, function(err, rows) {
-    if (err) return cb(err);
+function all() {
+  var options = {
+    key: DOC_TYPES.productProfile,
+    include_docs: true
+  };
+  return dbService.queryBy(VIEWS.byDocTypes, options);
+}
 
-    return cb(null, utility.removeDesignDocs(rows.toArray()));
-  });
+function get (id) {
+  return dbService.get(id);
+}
+
+function create (doc) {
+  doc.doc_type = DOC_TYPES.productProfile;
+  doc._id = dbService.getIdFrom(DOC_TYPES.productProfile, doc.name);
+  return dbService.create(doc);
+}
+
+function save (doc) {
+  doc.doc_type = DOC_TYPES.productProfile;
+  return dbService.save(doc);
 }
